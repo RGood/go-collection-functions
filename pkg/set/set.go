92224a8b693d3ef837269c053c6d3ba7a39785d6
RGood <mullan17@gmail.com>
2022-03-20 00:34:39 -0400

    Add set data types

--- pkg/set/set.go
+++ pkg/set/set.go
@@ -21,3 +21,102 @@ func (s *Set[K]) Contains(value K) bool {
 
 	return ok
 }
+
+type LinkedList[K comparable] struct {
+	head *Node[K]
+	tail *Node[K]
+	size int
+}
+
+func NewLinkedList[K comparable]() *LinkedList[K] {
+	return &LinkedList[K]{size: 0}
+}
+
+func (ll *LinkedList[K]) Size() int {
+	return ll.size
+}
+
+func (ll *LinkedList[K]) Head() *Node[K] {
+	return ll.head
+}
+
+func (ll *LinkedList[K]) Tail() *Node[K] {
+	return ll.tail
+}
+
+func (ll *LinkedList[K]) Append(value K) *Node[K] {
+	node := &Node[K]{Value: value}
+
+	if ll.size == 0 {
+		ll.head = node
+		ll.tail = node
+	} else {
+		ll.tail.Next = node
+		node.Prev = ll.tail
+		ll.tail = node
+	}
+
+	ll.size += 1
+
+	return node
+}
+
+func (ll *LinkedList[K]) Remove(node *Node[K]) {
+	prev := node.Prev
+	next := node.Next
+
+	if prev != nil {
+		prev.Next = next
+	}
+
+	if next != nil {
+		next.Prev = prev
+	}
+}
+
+type Node[K comparable] struct {
+	Value K
+	Next  *Node[K]
+	Prev  *Node[K]
+}
+
+type OrderedSet[K comparable] struct {
+	data     map[K]*Node[K]
+	dataList *LinkedList[K]
+}
+
+func NewOrderedSet[K comparable]() *OrderedSet[K] {
+	return &OrderedSet[K]{
+		data:     map[K]*Node[K]{},
+		dataList: NewLinkedList[K](),
+	}
+}
+
+func (os *OrderedSet[K]) Add(value K) {
+	node := os.dataList.Append(value)
+	os.data[value] = node
+}
+
+func (os *OrderedSet[K]) Remove(value K) {
+	node, ok := os.data[value]
+	if ok {
+		os.dataList.Remove(node)
+	}
+
+	delete(os.data, value)
+}
+
+func (os *OrderedSet[K]) Contains(value K) bool {
+	_, ok := os.data[value]
+	return ok
+}
+
+func (os *OrderedSet[K]) ForEach(callback func(int, K)) {
+	index := 0
+	cur := os.dataList.head
+	for cur != nil {
+		callback(index, cur.Value)
+		index++
+		cur = cur.Next
+	}
+}
--- pkg/set/set_test.go
+++ pkg/set/set_test.go
@@ -9,6 +9,30 @@ import (
 func TestSet(t *testing.T) {
 	s := NewSet[int]()
 	s.Add(1)
+	s.Add(75)
+	s.Add(69)
 
 	assert.True(t, s.Contains(1))
+	assert.True(t, s.Contains(75))
+	assert.True(t, s.Contains(69))
+	assert.False(t, s.Contains(99))
+}
+
+func TestOrderedSet(t *testing.T) {
+	s := NewOrderedSet[int]()
+	s.Add(8)
+	s.Add(1)
+	s.Add(63)
+	s.Add(42)
+
+	s.Remove(63)
+
+	s.Add(17)
+
+	entries := []int{}
+	s.ForEach(func(_, entry int) {
+		entries = append(entries, entry)
+	})
+
+	assert.Equal(t, entries, []int{8, 1, 42, 17})
 }

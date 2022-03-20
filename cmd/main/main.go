51301b13ea0c1f38391edb155af64edf7b356cae
RGood <mullan17@gmail.com>
2022-03-20 00:47:06 -0400

    Update package names

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/RGood/go-collection-functions/pkg/collections"
+	"github.com/RGood/go-collection-functions/pkg/collection"
 )
 
 func main() {
 	elements := []int{1, 2, 3, 4, 5}
 
-	newElements := collections.Map(elements, func(e int) int {
+	newElements := collection.Map(elements, func(e int) int {
 		return e * 2
 	})
 
 	println(
 		strings.Join(
-			collections.Map(newElements, func(e int) string {
+			collection.Map(newElements, func(e int) string {
 				return fmt.Sprintf("%d", e)
 			}),
 			", ",
